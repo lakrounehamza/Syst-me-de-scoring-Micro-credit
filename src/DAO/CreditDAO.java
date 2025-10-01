bff711f1627aaa42c3caea9f732aed5f2b25dafe
lakrounehamza <lakrounehamza10@gmail.com>
2025-10-01 16:26:37 +0100

    feat(DAO) :  implement  modification  et suprission de credit

--- src/DAO/CreditDAO.java
+++ src/DAO/CreditDAO.java
@@ -34,5 +34,29 @@ public class CreditDAO {
             System.out.println("Erreur SQL : " + e.getMessage());
         }
     }
-
+    public  void upDateCredit(Credit   credit){
+        try(Connection  connection  = ConnectionDB.getInstance().getConnection()){
+            PreparedStatement stmt = connection.prepareStatement("update credits set dateDeCredit =? ,montantDemande=?,montantOctroye=?,tauxInteret=?,dureeEnMois=?,typeCredit=?,decision=? where  id = ?");
+            stmt.setString(1,credit.getDateDeCredit().toString());
+            stmt.setDouble(2,credit.getMontantDemande());
+            stmt.setDouble(3,credit.getMontantOctroye());
+            stmt.setDouble(4,credit.getTauxInteret());
+            stmt.setInt(5,credit.getDureeEnMois());
+            stmt.setString(6,credit.getTypeCredit());
+            stmt.setString(7,credit.getDecision());
+            stmt.executeUpdate();
+            System.out.println("modification  avec succes !");
+        }catch(SQLException  e){
+            System.out.println("SQL  error "+e.getMessage());
+        }
+    }
+    public void deleteCredit(String id){
+        try(Connection  connection   = ConnectionDB.getInstance().getConnection()){
+            PreparedStatement  stmt  = connection.prepareStatement("delete from credits where  id =?");
+            stmt.setString(1,id);
+            stmt.executeUpdate();
+        }catch (SQLException e){
+            System.out.println("SQL  erorr : "+e.getMessage());
+        }
+    }
 }
