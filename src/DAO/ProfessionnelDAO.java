9111a9eb156e98c2bb04e93c4798b7339037249f
lakrounehamza <lakrounehamza10@gmail.com>
2025-10-01 14:15:10 +0100

    feat(DAO): ajout la suppression de professionnels

--- src/DAO/ProfessionnelDAO.java
+++ src/DAO/ProfessionnelDAO.java
@@ -1,4 +1,46 @@
 package DAO;
 
+import model.ConnectionDB;
+import model.Professionnel;
+
+import java.sql.*;
+
 public class ProfessionnelDAO {
+    public ProfessionnelDAO() {
+    }
+
+    public void addProfessionnel(Professionnel professionnel) {
+        try (Connection connection = ConnectionDB.getInstance().getConnection()) {
+            PreparedStatement stmt = connection.prepareStatement("CALL insert_personne_professionnel(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+            stmt.setString(1, professionnel.getId().toString());
+            stmt.setString(2, professionnel.getNom());
+            stmt.setString(3, professionnel.getPrenom());
+            stmt.setDate(4, Date.valueOf(professionnel.getDatedenaissance()));
+            stmt.setString(5, professionnel.getVille());
+            stmt.setInt(6, professionnel.getNombreEnfants());
+            stmt.setString(7, professionnel.getInvestissement());
+            stmt.setString(8, professionnel.getPlacement());
+            stmt.setString(9, professionnel.getSituation_familiale());
+            stmt.setInt(10, professionnel.getScore());
+            stmt.setDouble(11, professionnel.getRevenu());
+            stmt.setString(12, professionnel.getImmatriculationfiscale());
+            stmt.setString(13, professionnel.getSecteuractivite());
+            stmt.setString(14, professionnel.getActivite());
+
+            stmt.executeUpdate();
+            System.out.println("insersion  avec  secces ");
+        } catch (SQLException e) {
+            System.out.println("SQL  erroe " + e.getMessage());
+        }
+    }
+    public  void   deleteProfessionnel(String  id ){
+        try(Connection  connection   = ConnectionDB.getInstance().getConnection()){
+            PreparedStatement stmt  = connection.prepareStatement("delete   from   professionnels  whre  id  = ?");
+            stmt.setString(1,id);
+            stmt.executeUpdate();
+        }catch(SQLException  exception){
+            System.out.println(exception.getMessage());
+        }
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 import DAO.EmployeDAO;
+import DAO.ProfessionnelDAO;
 import model.Employe;
-import  java.time.*;
+import model.Professionnel;
+
+import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.stream.Stream;
@@ -9,11 +12,35 @@ import java.util.stream.Stream;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        EmployeDAO  d  =  new EmployeDAO();
+        EmployeDAO d = new EmployeDAO();
         LocalDate date = LocalDate.parse("2024-02-20");
 
-        Employe e1 = new Employe(
-                "Dupont",
+//        Employe e1 = new Employe(
+//                "Dupont",
+//                "Jean",
+//                LocalDate.parse("2024-02-20"),
+//                "Paris",
+//                2,
+//                "Immobilier",
+//                "Assurance Vie",
+//                "Marié",
+//                "2025-10-01",
+//                750,
+//                3500.75,
+//                 LocalDate.parse("2024-02-20"),
+//                "Développeur Senior",
+//                "CDI",
+//                "Informatique"
+//        );
+
+//         System.out.println(e1.toString());
+
+        //d.addEmploye(e1);
+        // d.deleteEmploye("f24b1050-32ba-4c11-ae3d-705ecfbea32a");
+        //d.getAllEmploye().forEach(employe -> System.out.println(employe.toString()));
+        ProfessionnelDAO  pro =  new ProfessionnelDAO();
+        Professionnel pro1 = new Professionnel(
+                "dupont",
                 "Jean",
                 LocalDate.parse("2024-02-20"),
                 "Paris",
@@ -23,16 +50,11 @@ public class Main {
                 "Marié",
                 "2025-10-01",
                 750,
-                3500.75,
-                 LocalDate.parse("2024-02-20"),
-                "Développeur Senior",
-                "CDI",
-                "Informatique"
+                "FR123456789",
+                "Informatique",
+                "Développement logiciel"
         );
-//         System.out.println(e1.toString());
-        //d.addEmploye(e1);
-       // d.deleteEmploye("f24b1050-32ba-4c11-ae3d-705ecfbea32a");
-        d.getAllEmploye().forEach(employe -> System.out.println(employe.toString()));
-
+        System.out.println(pro1);
+        pro.addProfessionnel(pro1);
     }
 }
