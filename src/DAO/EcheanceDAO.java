42081e215cc8616ae5722365f116793fba5f1510
lakrounehamza <lakrounehamza10@gmail.com>
2025-10-01 18:20:02 +0100

    feat(DAO) :  implement   ajoute credit

--- src/DAO/EcheanceDAO.java
+++ src/DAO/EcheanceDAO.java
@@ -1,4 +1,35 @@
 package DAO;
 
+import model.ConnectionDB;
+import model.Credit;
+import model.Echeance;
+
+import java.sql.Connection;
+import java.sql.*;
+
 public class EcheanceDAO {
+    public  EcheanceDAO(){}
+    public void addCredit(Credit credit, String idPersonne) {
+        try (Connection connection = ConnectionDB.getInstance().getConnection()) {
+            String sql = "INSERT INTO credits (id, idpersonne, dateDeCredit, montantDemande, montantOctroye, " +
+                    "tauxInteret, dureeEnMois, typeCredit, decision) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setString(1, credit.getId().toString());
+            stmt.setString(2, idPersonne);
+            stmt.setDate(3, Date.valueOf(credit.getDateDeCredit()));
+            stmt.setDouble(4, credit.getMontantDemande());
+            stmt.setDouble(5, credit.getMontantOctroye());
+            stmt.setDouble(6, credit.getTauxInteret());
+            stmt.setInt(7, credit.getDureeEnMois());
+            stmt.setString(8, credit.getTypeCredit());
+            stmt.setString(9, credit.getDecision().toString());
+
+            stmt.executeUpdate();
+            System.out.println(" Crédit ajouté avec succès !");
+        } catch (SQLException e) {
+            System.out.println("SQL error: " + e.getMessage());
+        }
+    }
+
 }
