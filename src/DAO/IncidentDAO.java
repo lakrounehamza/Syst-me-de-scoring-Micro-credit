662b530ef38e6923a4764847161b5cdee65fddd6
lakrounehamza <lakrounehamza10@gmail.com>
2025-10-01 19:08:19 +0100

    feat(DAO)   :  implementer insertion des  incidents

--- src/DAO/IncidentDAO.java
+++ src/DAO/IncidentDAO.java
@@ -1,4 +1,24 @@
 package DAO;
 
+import model.ConnectionDB;
+import model.Incident;
+
+import java.sql.*;
+
 public class IncidentDAO {
+    public  IncidentDAO(){}
+    public void addIncident(Incident incident,String idEcheance){
+        try(Connection  connection   = ConnectionDB.getInstance().getConnection()) {
+            PreparedStatement stmt = connection.prepareStatement("insert  into incidents   values(?,?,?,?,?,?)");
+            stmt.setString(1,incident.getId().toString());
+            stmt.setString(2,idEcheance);
+            stmt.setDate(3, Date.valueOf(incident.getDateIncident().toString()));
+            stmt.setString(4,incident.getEcheance());
+            stmt.setInt(5,incident.getScore());
+            stmt.setString(6,incident.getTypeincident().toString());
+            stmt.executeUpdate();
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
 }
